94508a1427a9a7fede9963d6fed2b4f7531572d5
GondekNP <GondekNP@gmail.com>
2021-12-07 15:03:38 -0600

    Autocorrelation and exogenous predictors

--- fit_model.py
+++ fit_model.py
@@ -4,33 +4,33 @@ import statsmodels.api as sm
 
 def fit_TWFE(data, treatment_years, target, linear_time = False, treat_dummy_type = 'invariant'):
     encoder = OneHotEncoder(handle_unknown='ignore')
-    id_match = data.set_index(['State','t']).index
+    idx_match = data.index
+    data = data.reset_index()
 
     state_encoder = encoder.fit(data[['State']])
     state_array = encoder.transform(data[['State']]).toarray()
     state_labels = state_encoder.get_feature_names(['state'])
     state_sparse = pd.DataFrame(state_array[:,1:],
                                 columns=state_labels[1:],
-                                index = id_match)
+                                index = idx_match)
 
     time_encoder = encoder.fit(data[['t']])
     time_array = encoder.transform(data[['t']]).toarray()
     time_labels = time_encoder.get_feature_names(['t'])
     time_sparse = pd.DataFrame(time_array[:,1:],
                                 columns=time_labels[1:],
-                                index = id_match)
-
+                                index = idx_match)
     comp = treatment_years.join(time_sparse).reset_index('t')
 
     comp['d'] = (comp['t'] >= comp['treatment_year']).apply(int)
     comp = comp.set_index('t',append = True).drop(columns = 'treatment_year')
-    
+
     staggered = sum(~pd.isna(pd.unique(treatment_years['treatment_year']))) != 1
     baseline = [state_labels[0]]
 
     if linear_time:
         time_out = data[['t']]
-        time_out.index = id_match
+        time_out.index = idx_match
     else:
         time_out = time_sparse
         baseline.append(time_labels[0])
@@ -48,9 +48,9 @@ def fit_TWFE(data, treatment_years, target, linear_time = False, treat_dummy_typ
             d_mat.columns = d_mat.columns.str.replace('state','d')
         data_out = pd.concat([state_sparse, time_out, d_mat], axis = 1)
 
-    target = data.set_index(['State','t']).loc[:,target]
+    target = data.loc[:,target]
+    target.index = idx_match
     X = sm.add_constant(data_out)
-    # X = data_out
     lm_FixedEffect = sm.OLS(target, X)
     results = lm_FixedEffect.fit()
 
--- gen_data.py
+++ gen_data.py
@@ -4,26 +4,32 @@ import pandas as pd
 
 sim_params =  {
             'TARGET' : 'GDPcont_t0',
-            'N_YEARS' : 50,
-            'N_TREATED' : 25,
-            'N_STATES' : 50,
-            'TREATMENT_YEAR' : {'mu' : 25, 
+            'SPATIAL_AUTOCORRELATION_PCT' : 0,
+            'N_YEARS' : 10,
+            'N_TREATED' : 5,
+            'N_STATES' : 10,
+            'TREATMENT_YEAR' : {'mu' : 5, 
                                 'sigma' : 0},
-            'SECTOR_GROWTH_PCT' : {'mu' : .02, 
-                                'sigma' : .01},                   
+            'EXOG_GROWTH_PCT' : {'mu' : .02, 
+                                'sigma' : .0005},
+            'EXOG_CONTROL_PCT_EXPLAINED': {'sigma' : .3},
+            'SECTOR_GROWTH_PCT' : {'sigma' : 0},                   
             'SECTOR_GROWTH_PCT_PREDICTORS' : {},
-            'TREATMENT_EFFECT' : {'mu': .06,
-                                  'sigma' : 0.02},
+            'TREATMENT_EFFECT' : {'mu': .3,
+                                  'sigma' : 0.002},
             'TREATMENT_EFFECT_PREDICTORS' : {} 
         }
 
 
 def sim_data(data, TARGET, N_YEARS, N_TREATED, N_STATES, TREATMENT_YEAR, 
+             EXOG_GROWTH_PCT, EXOG_CONTROL_PCT_EXPLAINED,
+             SPATIAL_AUTOCORRELATION_PCT,
              SECTOR_GROWTH_PCT, SECTOR_GROWTH_PCT_PREDICTORS,
              TREATMENT_EFFECT, TREATMENT_EFFECT_PREDICTORS):
 
     data = data.sample(n = N_STATES).sort_values(TARGET, ascending=False)
-    
+    # states = data.reset_index().State
+
     treated = np.array([np.repeat([np.nan], N_STATES - N_TREATED),
                         np.repeat([1],      N_TREATED)]).flatten()
     np.random.shuffle(treated)
@@ -32,34 +38,75 @@ def sim_data(data, TARGET, N_YEARS, N_TREATED, N_STATES, TREATMENT_YEAR,
     data.loc[:,'treatment_year'] = np.random.normal(TREATMENT_YEAR['mu'],
                                                     TREATMENT_YEAR['sigma'],
                                                     N_STATES) 
-    data.loc[:,'treatment_year'] = data.loc[:,'treatment_year'].astype(int) * treated                                             
+    data.loc[:,'treatment_year'] = data.loc[:,'treatment_year'].astype(int) * treated
 
-    for i_Year in range(1, N_YEARS + 1): # I think due to the fact that this is a time-series, there isn't a way to vectorize...
-        # dummy_treated_t = (i_Year > data['treatment_year']).apply(int)
-        dummy_treated_t = (i_Year == data['treatment_year']).apply(int)
-        treatment_effects = np.random.normal(TREATMENT_EFFECT['mu'],
+    treatment_effects = np.random.normal(TREATMENT_EFFECT['mu'],
                                              TREATMENT_EFFECT['sigma'],
                                              N_STATES)
-
-        if len(TREATMENT_EFFECT_PREDICTORS) != 0:
-            for cov, delta in TREATMENT_EFFECT_PREDICTORS.items():
-                treatment_effects += treatment_effects[cov] * delta
-
-        # this growth percent is the percentage in tech growth shared by all states
-        growth_pcts = np.random.normal(SECTOR_GROWTH_PCT['mu'],
-                                       SECTOR_GROWTH_PCT['sigma'],
-                                       N_STATES)
         
+    if len(TREATMENT_EFFECT_PREDICTORS) != 0:
+        for cov, delta in TREATMENT_EFFECT_PREDICTORS.items():
+            treatment_effects += treatment_effects[cov] * delta
+
+    data.loc[:,'treatment_effect'] = treatment_effects
+
+    df_treat = data[['treatment_year','treatment_effect']]
+    df_out = pd.DataFrame()
+
+    for i_Year in range(0, N_YEARS + 1): # I think due to the fact that this is a time-series, there isn't a way to vectorize...
+        df_long = pd.DataFrame()
+        dummy_treated_t = (i_Year == data['treatment_year']).apply(int)
+
+        # this growth percentage is the mean of each individual state's growth percentage
+        # can be thought of as the exogenous component to state level growth - ie, condition
+        # of the global economy during year i
+        i_Year_baseGrowth = np.random.normal(EXOG_GROWTH_PCT['mu'],
+                                             EXOG_GROWTH_PCT['sigma'],
+                                             1)
+
+        # this growth percent varies by state, but drawn from the same distribution centered
+        # at the exogenous growth percentage drawn above
+        growth_pcts = pd.Series(np.random.normal(i_Year_baseGrowth,
+                                           SECTOR_GROWTH_PCT['sigma'],
+                                           N_STATES), index=data.index)
+        if SPATIAL_AUTOCORRELATION_PCT != 0:
+            # if autocorrelated, take the random growth percents generated, and 
+            # make them all closer to their mean by region. If autocorrelation is 
+            # very high, ie 1, then all states in the region will have the same rate. 
+            # if 0, all states are indepedent and region has no effect.
+            autocor = data.loc[:,['Region']]
+            autocor['growth_pcts'] = growth_pcts
+            bunched = autocor.merge(autocor.groupby('Region').mean().reset_index(),
+                                on = 'Region',
+                                suffixes=('_state','_region'),
+                                ).set_index(autocor.index)
+            growth_pcts = \
+                bunched.loc[:,'growth_pcts_state'] * (1 - SPATIAL_AUTOCORRELATION_PCT) + \
+                bunched.loc[:,'growth_pcts_region'] * SPATIAL_AUTOCORRELATION_PCT
+        
+        # This is an index for endogenous variables that help us explain the growth rate
+        # for a particular state - if the sigma here is 0, this means that we can explain
+        # state level growth entirely since controls == growth pct
+        pct_explained = np.random.normal(1,
+                            EXOG_CONTROL_PCT_EXPLAINED['sigma'],
+                            N_STATES)
+        df_long.loc[:,'stateControls_t'] = growth_pcts * pct_explained
+
         if len(SECTOR_GROWTH_PCT_PREDICTORS) != 0:
             for cov, delta in SECTOR_GROWTH_PCT_PREDICTORS.items():
-                treatment_effects += treatment_effects[cov] * delta
+                treatment_effects += data[treatment_effects[cov]] * delta
 
-        growth_pcts = growth_pcts + (treatment_effects * dummy_treated_t)
-        data.loc[:, 'GDPcont_t' + str(i_Year)] = \
-            data.loc[:, 'GDPcont_t' + str(i_Year - 1)] + growth_pcts
+        if i_Year > 0:
+            growth_pcts_treated = growth_pcts + (treatment_effects * dummy_treated_t)
+            new_GDPcont = last_year_GDPcont + growth_pcts_treated
+            df_long.loc[:, 'GDPcont_t'] = new_GDPcont
+            last_year_GDPcont = new_GDPcont
+        else:
+            df_long.loc[:, 'GDPcont_t'] = data.loc[:,'GDPcont_t0']
+            last_year_GDPcont = data.loc[:,'GDPcont_t0']
+        
+        df_long.loc[:,'t'] = i_Year
+        df_out = pd.concat([df_out, df_long])
 
-        # data.loc[:, 'stateGDP_t' + str(i_Year)] = \
-        #     data.loc[:, 'stateGDP_t' + str(i_Year - 1)] * (1 + growth_pcts)
-        # data.loc[:, 'stateGDPpct_t' + str(i_Year)] = growth_pcts
-    
-    return data
+    df_out.set_index('t', append=True, inplace=True)
+    return df_out, df_treat
