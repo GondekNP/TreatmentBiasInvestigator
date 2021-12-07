73ac5055a0404bde1a505bd86c6ef152f8a37b08
GondekNP <GondekNP@gmail.com>
2021-12-06 22:12:06 -0600

    tech gdp pct

--- fit_model.py
+++ fit_model.py
@@ -2,32 +2,41 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 import statsmodels.api as sm
 
-def preprocess_dat(data, treatment_years, target, treat_dummy_type = 'invariant'):
+def fit_TWFE(data, treatment_years, target, linear_time = False, treat_dummy_type = 'invariant'):
     encoder = OneHotEncoder(handle_unknown='ignore')
     id_match = data.set_index(['State','t']).index
 
     state_encoder = encoder.fit(data[['State']])
     state_array = encoder.transform(data[['State']]).toarray()
     state_labels = state_encoder.get_feature_names(['state'])
-    state_sparse = pd.DataFrame(state_array,
-                                columns=state_labels,
+    state_sparse = pd.DataFrame(state_array[:,1:],
+                                columns=state_labels[1:],
                                 index = id_match)
 
     time_encoder = encoder.fit(data[['t']])
     time_array = encoder.transform(data[['t']]).toarray()
     time_labels = time_encoder.get_feature_names(['t'])
-    time_sparse = pd.DataFrame(time_array,
-                                columns=time_labels,
+    time_sparse = pd.DataFrame(time_array[:,1:],
+                                columns=time_labels[1:],
                                 index = id_match)
 
     comp = treatment_years.join(time_sparse).reset_index('t')
-    comp['d'] = (comp['t'] > comp['treatment_year']).apply(int)
+
+    comp['d'] = (comp['t'] >= comp['treatment_year']).apply(int)
     comp = comp.set_index('t',append = True).drop(columns = 'treatment_year')
     
     staggered = sum(~pd.isna(pd.unique(treatment_years['treatment_year']))) != 1
+    baseline = [state_labels[0]]
+
+    if linear_time:
+        time_out = data[['t']]
+        time_out.index = id_match
+    else:
+        time_out = time_sparse
+        baseline.append(time_labels[0])
 
     if treat_dummy_type == 'invariant' or not staggered:
-        data_out = pd.concat([state_sparse, time_sparse, comp.d], axis = 1)
+        data_out = pd.concat([state_sparse, time_out, comp.d], axis = 1)
     else: #staggered treatment
         if treat_dummy_type == 'time_variant':
             d_mat = comp.iloc[:,:-1].apply(lambda x: x*comp.d)
@@ -37,13 +46,13 @@ def preprocess_dat(data, treatment_years, target, treat_dummy_type = 'invariant'
             d_mat = state_sparse.apply(lambda x: x*comp.d)
             d_mat = d_mat.loc[:,d_mat.sum(axis = 0)!=0]
             d_mat.columns = d_mat.columns.str.replace('state','d')
-        data_out = pd.concat([state_sparse, time_sparse, d_mat], axis = 1)
+        data_out = pd.concat([state_sparse, time_out, d_mat], axis = 1)
 
     target = data.set_index(['State','t']).loc[:,target]
     X = sm.add_constant(data_out)
+    # X = data_out
     lm_FixedEffect = sm.OLS(target, X)
     results = lm_FixedEffect.fit()
-    baseline = (state_labels[0], time_labels[0])
 
     return results, data_out, baseline
     # return data_out
--- gen_data.py
+++ gen_data.py
@@ -3,25 +3,26 @@ import random
 import pandas as pd
 
 sim_params =  {
-            'N_YEARS' : 8,
-            'N_TREATED' : 2,
-            'N_STATES' : 4,
-            'TREATMENT_YEAR' : {'mu' : 4, 
-                                'sigma' : 1},
-            'GDP_GROWTH_PCT' : {'mu' : .02, 
-                                'sigma' : .001},                   
-            'GDP_GROWTH_PCT_PREDICTORS' : {},
-            'TREATMENT_EFFECT' : {'mu': .5,
-                                  'sigma' : .0001},
+            'TARGET' : 'GDPcont_t0',
+            'N_YEARS' : 50,
+            'N_TREATED' : 25,
+            'N_STATES' : 50,
+            'TREATMENT_YEAR' : {'mu' : 25, 
+                                'sigma' : 0},
+            'SECTOR_GROWTH_PCT' : {'mu' : .02, 
+                                'sigma' : .01},                   
+            'SECTOR_GROWTH_PCT_PREDICTORS' : {},
+            'TREATMENT_EFFECT' : {'mu': .06,
+                                  'sigma' : 0.02},
             'TREATMENT_EFFECT_PREDICTORS' : {} 
         }
 
 
-def sim_data(data, N_YEARS, N_TREATED, N_STATES, TREATMENT_YEAR, 
-             GDP_GROWTH_PCT, GDP_GROWTH_PCT_PREDICTORS,
+def sim_data(data, TARGET, N_YEARS, N_TREATED, N_STATES, TREATMENT_YEAR, 
+             SECTOR_GROWTH_PCT, SECTOR_GROWTH_PCT_PREDICTORS,
              TREATMENT_EFFECT, TREATMENT_EFFECT_PREDICTORS):
 
-    data = data.sample(n = N_STATES).sort_values('stateGDP_t0', ascending=False)
+    data = data.sample(n = N_STATES).sort_values(TARGET, ascending=False)
     
     treated = np.array([np.repeat([np.nan], N_STATES - N_TREATED),
                         np.repeat([1],      N_TREATED)]).flatten()
@@ -34,7 +35,8 @@ def sim_data(data, N_YEARS, N_TREATED, N_STATES, TREATMENT_YEAR,
     data.loc[:,'treatment_year'] = data.loc[:,'treatment_year'].astype(int) * treated                                             
 
     for i_Year in range(1, N_YEARS + 1): # I think due to the fact that this is a time-series, there isn't a way to vectorize...
-        dummy_treated_t = (i_Year > data['treatment_year']).apply(int)
+        # dummy_treated_t = (i_Year > data['treatment_year']).apply(int)
+        dummy_treated_t = (i_Year == data['treatment_year']).apply(int)
         treatment_effects = np.random.normal(TREATMENT_EFFECT['mu'],
                                              TREATMENT_EFFECT['sigma'],
                                              N_STATES)
@@ -43,18 +45,21 @@ def sim_data(data, N_YEARS, N_TREATED, N_STATES, TREATMENT_YEAR,
             for cov, delta in TREATMENT_EFFECT_PREDICTORS.items():
                 treatment_effects += treatment_effects[cov] * delta
 
-        growth_pcts = np.random.normal(GDP_GROWTH_PCT['mu'],
-                                       GDP_GROWTH_PCT['sigma'],
+        # this growth percent is the percentage in tech growth shared by all states
+        growth_pcts = np.random.normal(SECTOR_GROWTH_PCT['mu'],
+                                       SECTOR_GROWTH_PCT['sigma'],
                                        N_STATES)
         
-        if len(GDP_GROWTH_PCT_PREDICTORS) != 0:
-            for cov, delta in GDP_GROWTH_PCT_PREDICTORS.items():
+        if len(SECTOR_GROWTH_PCT_PREDICTORS) != 0:
+            for cov, delta in SECTOR_GROWTH_PCT_PREDICTORS.items():
                 treatment_effects += treatment_effects[cov] * delta
 
         growth_pcts = growth_pcts + (treatment_effects * dummy_treated_t)
+        data.loc[:, 'GDPcont_t' + str(i_Year)] = \
+            data.loc[:, 'GDPcont_t' + str(i_Year - 1)] + growth_pcts
 
-        data.loc[:, 'stateGDP_t' + str(i_Year)] = \
-            data.loc[:, 'stateGDP_t' + str(i_Year - 1)] * (1 + growth_pcts)
-        data.loc[:, 'stateGDPpct_t' + str(i_Year)] = growth_pcts
+        # data.loc[:, 'stateGDP_t' + str(i_Year)] = \
+        #     data.loc[:, 'stateGDP_t' + str(i_Year - 1)] * (1 + growth_pcts)
+        # data.loc[:, 'stateGDPpct_t' + str(i_Year)] = growth_pcts
     
     return data
